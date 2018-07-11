93aafbe1bd07412992a33ec9d5fdd0d1b19c906a
Jerry Padilla <jerrypadilla23@gmail.com>
2018-07-11 00:30:48 -0500

    Solved

---
+++ bcrypt_python3_issue.py
@@ -0,0 +1,23 @@
+import bcrypt
+
+# work around for storing my hashed passwords in the db. I decode('utf-8') the hashed PW then store it in the db. Because when validating the hashed pw on login it continues to throw a Invalid Salt error.
+db_new_passwordhash = bcrypt.hashpw(postData['password'].encode(), bcrypt.gensalt())
+            
+    User.objects.create(
+        first_name=postData['first_name'],
+        last_name=postData['last_name'],
+        email=postData['email'],
+        password=db_new_passwordhash.decode('utf-8'), # <<< magic 
+    )
+
+# validating the hashed pw
+user = User.objects.get(email=postData['email'])
+
+db_password = user.password
+
+if bcrypt.checkpw(postData['password'].encode(), db_password.encode()):
+    print('this worked')
+else:
+    print('it didnt work')
+
+# without decoding, it just keeps giving Invalid Salt. Any suggestions on possible problems would be great.
---
+++ readMe.md
@@ -0,0 +1,13 @@
+# Bcrypt Python3 Invalid Salt Issue fixed
+
+Found a way around this Invalid Salt issue that was plaguing me for hours. 
+
+virtualenv
+
+python 3.7.0
+Django 2.0.7
+
+
+## Contributing
+
+If you know something better. Share please.
